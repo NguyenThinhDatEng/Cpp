445133209c59b96b843f2fd5baf5db73dc1214a9
SnakeTZZO <toithienamxinnghe@gmail.com>
2021-05-09 15:28:52 +0700

    add files

--- array.cpp
+++ array.cpp
@@ -1,14 +1,29 @@
-// D://NguyenThinhDatSci//Cpp//HUST_IT3040_KTLT//3.02.txt
+// D://NguyenThinhDatSci//Cpp//HUST_IT3040_KTLT//3.03.txt
 #include "array.h"
 
-void initialize2Darray(int** (&a), int n)
+void initializeArr(int* (&a), int n)
+{
+	a = new int [n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = 0;
+	}
+}
+
+void initializeArr(int** (&a), int n)
 {
 	a = new int* [n + 1];
 	for (int i = 0; i < n + 1; i++)
 		*(a + i) = new int[n + 1];
 }
 
-void input2Darray(int** (&a), int n)
+void inputArr(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+void inputArr(int** a, int n)
 {
 	for (int i = 1; i <= n; i++)
 	{
@@ -17,9 +32,29 @@ void input2Darray(int** (&a), int n)
 	}
 }
 
+void readFile(int* (&a), int& n)
+{
+	ifstream inputFile("D://NguyenThinhDatSci//Cpp//HUST_IT3040_KTLT//3.04.txt");
+	if (inputFile.fail())
+	{
+		cout << "Failed to open this file!" << endl;
+		exit(0);
+	}
+	inputFile >> n;
+	cout << n << endl;
+	initializeArr(a, n);
+	while (!inputFile.eof())
+	{
+		for (int i = 0; i < n; i++)
+			inputFile >> a[i];
+	}
+	displayArr(a, n);
+	inputFile.close();
+}
+
 void readFile(int **(&a), int &n)
 {
-	ifstream inputFile("D://NguyenThinhDatSci//Cpp//HUST_IT3040_KTLT//3.02.txt");
+	ifstream inputFile("D://NguyenThinhDatSci//Cpp//HUST_IT3040_KTLT//3.03.txt");
 
 	if (inputFile.fail())
 	{
@@ -28,14 +63,14 @@ void readFile(int **(&a), int &n)
 	}
 	inputFile >> n;
 	cout << n << endl;
-	initialize2Darray(a, n);
+	initializeArr(a, n);
 	while (!inputFile.eof())
 	{
 		for (int i = 1; i <= n; i++)
 			for (int j = 1; j <= n; j++)
 				inputFile >> a[i][j];
 	}
-	display2Darray(a, n);
+	displayArr(a, n);
 	inputFile.close();
 }
 
@@ -45,7 +80,16 @@ void showDistanceTraveled(int* a, int n)
 		cout << " (" << a[i] << ") " << (i == (n - 1) ? "\n" : "->");
 }
 
-void display2Darray(int** a, int n)
+void displayArr(int* a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << setw(5) << right << a[i];
+	}
+	cout << endl;
+}
+
+void displayArr(int** a, int n)
 {
 	for (int i = 1; i <= n; i++)
 	{
@@ -53,6 +97,11 @@ void display2Darray(int** a, int n)
 			cout << setw(5) << right << a[i][j];
 		cout << endl;
 	}
+}
+
+void freeArr(int* a)
+{
+	delete[] a;
 }
 
 void free2Darray(int** a, int n)
--- array.h
+++ array.h
@@ -6,14 +6,24 @@
 
 using namespace std;
 
-void initialize2Darray(int** (&a), int n);
+void initializeArr(int* (&a), int n);
 
-void input2Darray(int** (&a), int n);
+void initializeArr(int** (&a), int n);
+
+void inputArr(int* a, int n);
+
+void inputArr(int** a, int n);
+
+void readFile(int* (&a), int& n);
 
 void readFile(int** (&a), int &n);
 
 void showDistanceTraveled(int* a, int n);
 
-void display2Darray(int** a, int n);
+void displayArr(int* a, int n);
+
+void displayArr(int** a, int n);
+
+void freeArr(int* a);
 
 void free2Darray(int** a, int n);
