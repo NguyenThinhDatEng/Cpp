52c9fa3ab7c549503b18dc1fc1ec43917676f80b
SnakeTZZO <toithienamxinnghe@gmail.com>
2021-05-10 21:01:30 +0700

    add, modify

--- HUST_IT3040_KTLT/3.12.cpp
+++ HUST_IT3040_KTLT/3.12.cpp
@@ -1,116 +1,66 @@
-#include <iostream>
-#include <vector>
-
+#include<iostream>
+#include<vector>
 using namespace std;
+const int MAX = 100;
+int n, k;
+int m;
+vector<vector<int> > vt; // chuyen tu luu tru cac canh sang luu tru danh sach ke
+int x[MAX];
+bool visited[MAX];
+int res;
 
-int n;		// the number of vertices
-int k;		// the number of single paths
-int m;		// the number of sides
-vector<int> visited;	// to save the points are visited
-int** a;			// adjacency matrix
-vector< vector<int> > listPath;		// to save the paths are passed
+void input() {
+    cin >> n >> k;
+    cin >> m;
+    vt.resize(n + 1);
+    for (int i = 0; i < m; i++) {
+        int tmp1, tmp2;
+        cin >> tmp1 >> tmp2;
 
-void initializeArr(int** (&a), int n)
-{
-	a = new int* [n + 1];
-	for (int i = 0; i < n + 1; i++)
-		*(a + i) = new int[n + 1];
-	for (int i = 0; i < n + 1; i++)
-		for (int j = 0; j <= n; j++)
-			a[i][j] = 0;
+        vt[tmp1 - 1].push_back(tmp2 - 1); // dinh tmp2 ke voi dinh tmp1
+        vt[tmp2 - 1].push_back(tmp1 - 1); // dinh tmp1 ke voi dinh tmp2
+    }
+
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
+    res = 0;
 }
 
-void inputArr(int** a, int n)
-{
-	int i, j;		// row, column
-	int count = 0;	// from 0 to m
-	do {
-		cin >> i >> j;
-		a[i][j] = a[j][i] = 1;
-		count++;
-	} while (count < m);
+bool check(int a, int i) {
+    if (a == 0) return true;
+    if (visited[i]) return false;
+
+    int index = 0;
+    for (int j = 0; j < vt[x[a - 1]].size(); j++) {
+        if (i == vt[x[a - 1]][j]) index++;
+    }
+    if (index == 0) return false;
+
+
+    return true;
 }
 
-bool isCandidate(int i, int h)
-{
-	if (a[visited.at(h - 1)][i] == 0) return false;
-	for (int j = 0; j < h; j++)
-		if (visited.at(j) == i) return false;
-	return true;
+void solution() {
+    res++;
 }
 
-bool compareTwoPaths(vector<int> a, vector<int> b)
-{
-	for (int i = 0; i <= k; i++)
-		if (a.at(i) != b.at(k - i)) return true;
-	return false;
+void TRY(int a) {
+    for (int i = 0; i < n; i++) {
+        if (check(a, i)) {
+            visited[i] = true;
+            x[a] = i;
+
+            if (a == k) solution();
+            else TRY(a + 1);
+
+            visited[i] = false;
+        }
+    }
 }
 
-bool existedPath(vector<int> visited)
-{
-	for (vector<int> path : listPath)
-		if (compareTwoPaths(visited, path) == false)
-			return true;
-	return false;
+int main() {
+    input();
+    TRY(0);
+    cout << res / 2;
 }
-
-void solution()
-{
-	if (existedPath(visited)) return;
-	listPath.push_back(visited);
-}
-
-void TRY(int h)
-{
-	for (int i = 1; i <= n; i++)
-	{
-		if (isCandidate(i, h))
-		{
-			visited.push_back(i);
-			if (h == k)
-				solution();
-			else
-				TRY(h + 1);
-			visited.pop_back();
-		}
-	}
-}
-
-void free2Darray(int** a, int n)
-{
-	for (int i = 0; i <= n; i++)
-		delete[] * (a + i);
-	delete[] a;
-}
-
-int main()
-{
-	//cout << "\nNguyen Van Thinh 20194178\n" << endl;
-	cin >> n >> k;
-	cin >> m;
-
-	initializeArr(a, n);
-	inputArr(a, n);
-	
-	for (int i = 1; i <= n; i++)
-	{
-		visited.push_back(i);
-		TRY(1);
-		visited.clear();
-	}
-
-	//# display result
-	cout << listPath.size();
-
-	//# display all of paths
-	/*cout << endl;
-	for (int i = 0; i < listPath.size(); i++)
-	{
-		for (int j = 0; j <= k; j++)
-			cout << listPath.at(i).at(j) << " ";
-		cout << endl;
-	}*/
-
-	free2Darray(a, n);
-	return 0;
-} 
